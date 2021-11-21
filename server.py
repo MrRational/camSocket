55b1acf94a2c9529339a3e0ae17afe51b65b6d84
MrRational <90264475+MrRational@users.noreply.github.com>
2021-11-21 11:56:55 +0545

    Update server.py

--- server.py
+++ server.py
@@ -12,10 +12,9 @@ sock.bind((TCP_IP, TCP_PORT))
 sock.listen(5)
 
 while True:
-    (client_socket, client_address) = sock.accept() # wait for client
+    (client_socket, client_address) = sock.accept() # waiting  for client
     print ('connection established with ' +str(client_address))
     cap = cv2.VideoCapture(0)
-    # pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
     while True:
         flag, frame = cap.read()
         if flag:
@@ -24,15 +23,7 @@ while True:
             p = struct.pack('I', size)
             frame = p + frame
             client_socket.sendall(frame)
-        # else:
-        #     cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame-1)
-
-        # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-        #     size = 10
-        #     p = struct.pack("I", size)
-        #     client_socket.send(p)
-        #     client_socket.send(''.encode())
-        #     break
+        
 
 
 
